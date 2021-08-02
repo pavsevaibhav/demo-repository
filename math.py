8cfa0858e406d32acc547da49fa0cab61e7c0a68
pavsevaibhav <76787875+pavsevaibhav@users.noreply.github.com>
2021-08-02 14:30:02 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,18 @@
+#add funtion
+def add(x,y):
+    return x+y #on master branch
+#subtract function
+def subtract(x,y):
+    return x-y #changed by master 
+#multiple function
+def multiply(x,y):
+    if(x==0||y==0)
+        return 0;
+    else 
+        return x*y
+#divide function
+def divide(x,y):
+    if(y==0)
+        return divide by 0 error
+    else 
+        return x/y  #on Bug789 branch
